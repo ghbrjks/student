65892d00a9ab03cfa04eec03b4590acc2b73433f
KPOT <shadow.pixel47@gmail.com>
2023-11-11 13:22:34 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+ #include "std.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout<<"Insert first number: ";
+    cin>> a;
+    cout<<"Insert second number: ";
+    cin>> b;
+}
